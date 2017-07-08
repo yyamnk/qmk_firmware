ce74e80d7c21f1044d38581b5a4b8bbec2339d73
yyamnk <y.yamnk@gmail.com>
2017-07-08 13:07:34 +0900

    upd: hhkb/yyamnk, Left FN + Right FN + VimFN

--- keyboards/hhkb/keymaps/yyamnk/keymap.c
+++ keyboards/hhkb/keymaps/yyamnk/keymap.c
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
   [BASE] = KEYMAP(  //  default layer
-  KC_GRV  , KC_1 , KC_2 , KC_3 , KC_4   , KC_5 , KC_6 , KC_7   , KC_8    , KC_9   , KC_0    , KC_MINS , KC_EQL  , KC_BSPC , KC_ESC , \
-  KC_TAB  , KC_Q , KC_W , KC_E , KC_R   , KC_T , KC_Y , KC_U   , KC_I    , KC_O   , KC_P    , KC_LBRC , KC_RBRC , KC_BSLS , \
-  KC_LCTL , KC_A , KC_S , KC_D , KC_FN4 , KC_G , KC_H , KC_J   , KC_K    , KC_L   , KC_SCLN , KC_QUOT , KC_ENT  , \
-  KC_LSFT , KC_Z , KC_X , KC_C , KC_FN2 , KC_B , KC_N , KC_FN3 , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT , KC_FN1  , \
+  KC_GRV  , KC_1 , KC_2 , KC_3   , KC_4   , KC_5 , KC_6 , KC_7 , KC_8    , KC_9   , KC_0    , KC_MINS , KC_EQL  , KC_BSPC , KC_ESC , \
+  KC_TAB  , KC_Q , KC_W , KC_E   , KC_R   , KC_T , KC_Y , KC_U , KC_I    , KC_O   , KC_P    , KC_LBRC , KC_RBRC , KC_BSLS , \
+  KC_LCTL , KC_A , KC_S , KC_FN2 , KC_FN4 , KC_G , KC_H , KC_J , KC_FN3  , KC_L   , KC_SCLN , KC_QUOT , KC_ENT  , \
+  KC_LSFT , KC_Z , KC_X , KC_C   , KC_V   , KC_B , KC_N , KC_M , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT , KC_FN1  , \
   // KC_LALT , KC_LGUI , M(SPC_FN) , KC_RGUI , KC_RALT) ,
   // KC_LALT , KC_LGUI , KC_FN2 , KC_RGUI , KC_RALT) ,
   KC_LALT , KC_LGUI , KC_SPACE, KC_RGUI , KC_RALT) ,
@@ -101,8 +101,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LEFT_FN] = KEYMAP(
   KC_NO   , KC_F1   , KC_F2   , KC_F3   , KC_F4      , KC_F5 , KC_F6    , KC_F7      , KC_F8         , KC_F9        , KC_F10      , KC_F11  , KC_F12 , KC_NO , KC_NO , \
   KC_NO   , KC_NO   , KC_NO   , KC_NO   , LCTL(KC_R) , KC_NO , KC_NO    , LCTL(KC_U) , LCTL(KC_I)    , KC_NO        , LCTL(KC_P)  , KC_NO   , KC_NO  , KC_NO , \
-  KC_LCTL , KC_HOME , KC_NO   , KC_END  , LCTL(KC_F) , KC_NO , KC_DEL   , KC_BSPC    , LCTL(KC_K)    , LCTL(KC_L)   , KC_ESC      , KC_NO   , KC_NO  , \
-  KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_TRNS    , KC_NO , KC_NO    , KC_ENT     , LCTL(KC_COMM) , LCTL(KC_DOT) , M(DEL_WORD) , KC_RSFT , KC_NO  , \
+  KC_LCTL , KC_HOME , KC_NO   , KC_TRNS , LCTL(KC_F) , KC_NO , KC_DEL   , KC_BSPC    , LCTL(KC_K)    , LCTL(KC_L)   , KC_ESC      , KC_NO   , KC_NO  , \
+  KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_NO    , KC_NO , KC_NO    , KC_ENT     , LCTL(KC_COMM) , LCTL(KC_DOT) , M(DEL_WORD) , KC_RSFT , KC_NO  , \
   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS)   ,
 
 
@@ -131,10 +131,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      */
   [RGHT_FN] = KEYMAP(
-  KC_NO   , KC_F1   , KC_F2   , KC_F3   , KC_F4      , KC_F5 , KC_F6    , KC_F7      , KC_F8         , KC_F9        , KC_F10      , KC_F11  , KC_F12 , KC_NO , KC_NO , \
-  KC_NO   , KC_NO   , KC_NO   , KC_NO   , LCTL(KC_R) , KC_NO , KC_NO    , LCTL(KC_U) , LCTL(KC_I)    , KC_NO        , LCTL(KC_P)  , KC_NO   , KC_NO  , KC_NO , \
-  KC_LCTL , KC_HOME , KC_NO   , KC_END  , LCTL(KC_F) , KC_NO , KC_DEL   , KC_BSPC    , LCTL(KC_K)    , LCTL(KC_L)   , KC_ESC      , KC_NO   , KC_NO  , \
-  KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_TRNS    , KC_NO , KC_TRNS  , KC_TRNS    , LCTL(KC_COMM) , LCTL(KC_DOT) , M(DEL_WORD) , KC_RSFT , KC_NO  , \
+  KC_NO   , KC_F1   , KC_F2   , KC_F3   , KC_F4      , KC_F5 , KC_F6   , KC_F7      , KC_F8         , KC_F9        , KC_F10      , KC_F11  , KC_F12 , KC_NO , KC_NO , \
+  KC_NO   , KC_NO   , KC_NO   , KC_NO   , LCTL(KC_R) , KC_NO , KC_NO   , LCTL(KC_U) , LCTL(KC_I)    , KC_NO        , LCTL(KC_P)  , KC_NO   , KC_NO  , KC_NO , \
+  KC_LCTL , KC_HOME , KC_NO   , KC_END  , LCTL(KC_F) , KC_NO , KC_DEL  , KC_BSPC    , KC_TRNS       , LCTL(KC_L)   , KC_ESC      , KC_NO   , KC_NO  , \
+  KC_TRNS , KC_NO   , KC_NO   , KC_NO   , KC_TRNS    , KC_NO , KC_TRNS , KC_NO      , LCTL(KC_COMM) , LCTL(KC_DOT) , M(DEL_WORD) , KC_RSFT , KC_NO  , \
   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS)   ,
 
 
@@ -184,8 +184,10 @@ const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_MOMENTARY(HHKB),  // KC_FN1
     // [2] = ACTION_LAYER_TAP_KEY(SPACE_FN, KC_SPACE),  // KC_FN2
     // [2] = ACTION_LAYER_MOMENTARY(SPACE_FN),  // KC_FN2
-    [2] = ACTION_LAYER_TAP_KEY(LEFT_FN, KC_V),  // KC_FN2
-    [3] = ACTION_LAYER_TAP_KEY(RGHT_FN, KC_M),  // KC_FN3
+    // [2] = ACTION_LAYER_TAP_KEY(LEFT_FN, KC_V),  // KC_FN2
+    // [3] = ACTION_LAYER_TAP_KEY(RGHT_FN, KC_M),  // KC_FN3
+    [2] = ACTION_LAYER_TAP_KEY(LEFT_FN, KC_D),  // KC_FN2
+    [3] = ACTION_LAYER_TAP_KEY(RGHT_FN, KC_K),  // KC_FN3
     // [2] = ACTION_LAYER_MOMENTARY(SPACE_FN),  // KC_FN2
     [4] = ACTION_LAYER_TAP_KEY(VIM, KC_F),  // KC_FN3
 };
