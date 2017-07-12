5efd5e853dde40f0658ceb8c9218ebbbb1106a1b
yyamnk <y.yamnk@gmail.com>
2017-07-12 17:46:31 +0900

    upd: lets_split/yyamnk, ` to tapping ctrl

--- keyboards/lets_split/keymaps/yyamnk/keymap.c
+++ keyboards/lets_split/keymaps/yyamnk/keymap.c
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_TAB,  KC_Q,    KC_W,   KC_E,  KC_R,       KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, \
-  KC_LCTL, KC_A,    KC_S,   KC_D,  LT(_VIM_FN, KC_F),   KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,   KC_C,  KC_V,       KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LALT, KC_LGUI, KC_GRV, LOWER, KC_SPC,     UNIX_FN, UNIX_FN, KC_SPC, RAISE,   KC_BSLS, KC_RGUI, KC_RALT \
+  KC_GRV,        KC_Q,    KC_W,   KC_E,  KC_R,       KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, \
+  CTL_T(KC_TAB), KC_A,    KC_S,   KC_D,  LT(_VIM_FN, KC_F),   KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,       KC_Z,    KC_X,   KC_C,  KC_V,       KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+  KC_LALT,       KC_LGUI, KC_LGUI, LOWER, KC_SPC,     UNIX_FN, UNIX_FN, KC_SPC, RAISE,   KC_BSLS, KC_RGUI, KC_RALT \
 ),
 
 /* Dvorak
