43f0672685db0715ca3af3f9c4054416ee2a7168
yyamnk <y.yamnk@gmail.com>
2017-07-14 19:02:08 +0900

    upd: lets_split/yyamnk, LSPO-RSPC to LSFT-RSFT. prevent stacking

--- keyboards/lets_split/keymaps/yyamnk/keymap.c
+++ keyboards/lets_split/keymaps/yyamnk/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP( \
   KC_TAB,        KC_Q,    KC_W,    KC_E,  KC_R,       KC_T,   KC_Y,    KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, \
   CTL_T(KC_GRV), KC_A,    KC_S,    KC_D,  LT(_VIM_FN, KC_F),  KC_G,    KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSPO,       KC_Z,    KC_X,    KC_C,  KC_V,       KC_B,   KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
+  KC_LSFT,       KC_Z,    KC_X,    KC_C,  KC_V,       KC_B,   KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
   KC_LALT,       KC_LGUI, KC_LGUI, LOWER, NUM,        KC_SPC, UNIX_FN, NUM,  RAISE,   KC_BSLS, KC_RCTL, KC_RALT \
 ),
 
