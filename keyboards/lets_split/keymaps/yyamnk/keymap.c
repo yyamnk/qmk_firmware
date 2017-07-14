2427f182f3ee326922f1595d8a99ca374b4515e2
yyamnk <y.yamnk@gmail.com>
2017-07-14 17:50:50 +0900

    upd: lets_split/yyamnk, RALT to RCTL, for pointing device

--- keyboards/lets_split/keymaps/yyamnk/keymap.c
+++ keyboards/lets_split/keymaps/yyamnk/keymap.c
@@ -41,16 +41,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |`andC |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
+ * |(andS |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | )andS|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Alt  | GUI  | GUI  | Lower| NUM  |SpaceF|SpaceF| NUM  |Raise |  \   | GUI  | Alt  |
+ * | Alt  | GUI  | GUI  | Lower| NUM  |SpaceF|SpaceF| NUM  |Raise |  \   | GUI  | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,        KC_Q,    KC_W,    KC_E,  KC_R,       KC_T,   KC_Y,    KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, \
   CTL_T(KC_GRV), KC_A,    KC_S,    KC_D,  LT(_VIM_FN, KC_F),  KC_G,    KC_H, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSPO,       KC_Z,    KC_X,    KC_C,  KC_V,       KC_B,   KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
-  KC_LALT,       KC_LGUI, KC_LGUI, LOWER, NUM,        KC_SPC, UNIX_FN, NUM,  RAISE,   KC_BSLS, KC_RGUI, KC_RALT \
+  KC_LALT,       KC_LGUI, KC_LGUI, LOWER, NUM,        KC_SPC, UNIX_FN, NUM,  RAISE,   KC_BSLS, KC_RGUI, KC_RCTL \
 ),
 
 /* Dvorak
